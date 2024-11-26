db0870edb7d9f21ce6701eb47e53e3a9e7eb1528
zero@void <shinyzero0@gmail.com>
2024-11-26 21:23:33 +0300

    upd gen

--- test_files/gen.sh
+++ test_files/gen.sh
@@ -1,1 +1,2 @@
-for i in {1..1000}; do dd if=/dev/urandom of=./src/$(printf %05d $i).dat bs=1024 count=1024; done
+#!/usr/bin/env bash
+n=${1:-1000};mkdir -p src;for i in $(eval echo {1..$n}); do dd if=/dev/urandom of=./src/$(printf %05d $i).dat bs=1024 count=1024; done
